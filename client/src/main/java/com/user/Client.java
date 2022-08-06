2dea8eafc9b6f07ea30ccccf8dda51a71e2e4bac
Grande17 <mmarkowski955@gmail.com>
2022-08-06 11:35:17 +0200

    dodanie eureki, open feign i modolu client/user

---
+++ client/src/main/java/com/user/Client.java
@@ -0,0 +1,29 @@
+package com.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Client {
+
+    @Id
+    @SequenceGenerator(
+            name = "user_id_sequence",
+            sequenceName = "user_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_id_sequence"
+    )
+    private Integer id;
+    private String name;
+    private String surname;
+    private String email;
+    private String phoneNumber;
+}
